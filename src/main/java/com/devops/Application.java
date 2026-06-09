c2e267f9d841be6a244df50cbfe6404d29cb348a
ChaitraLG77 <chaitralg77@gmail.com>
2026-06-09 18:13:00 +0530

    improved home page

--- src/main/java/com/devops/Application.java
+++ src/main/java/com/devops/Application.java
@@ -10,14 +10,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
 
         @GetMapping("/")
-    public String home() {
-        return "Welcome to DevOps Project two 🚀";
-    }
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello DevOps 🚀";
-    }
+public String home() {
+    return """
+        <html>
+        <head>
+            <title>DevOps Project</title>
+        </head>
+        <body style="text-align:center; font-family:Arial; margin-top:100px;">
+            <h1 style="font-size:50px; color:#2E86C1;">
+                🚀 DevOps CI/CD Pipeline Project
+            </h1>
+            <h2 style="font-size:30px;">
+                Application Running Successfully ✅
+            </h2>
+            <p style="font-size:20px;">
+                Spring Boot | GitHub Actions | Docker | Docker Hub | Render
+            </p>
+        </body>
+        </html>
+        """;
+}
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
